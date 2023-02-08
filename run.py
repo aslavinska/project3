d3e310bf7c0bd0dc3a738f8c8da32fe97d4a07d1
aslavinska <anastasija.slavinska@avl.com>
2023-02-08 10:51:40 +0000

    Adding basic rules of the game

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
+import random
 import gspread
 from google.oauth2.service_account import Credentials
 
+# The SCOPE lists the APIs that the program should access in order to run.
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -12,8 +14,20 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('project3')
 
-sales = SHEET.worksheet('sales')
+records = SHEET.worksheet('records')
 
-data = sales.get_all_values()
+data = records.get_all_values()
 
-print(data)
+
+def main ():
+    """
+    Run all program functions
+    """
+
+print("Welcome to Rock, Paper, Scissors game. \n")
+print("Winnin rules of the game are: \n"
+    + "Paper beats rock \n" 
+    + "Rock beats scissors \n"
+    + "Scissors beats paper \n")
+
+main()
