3646a82facbbe0f1f702a34ba8c24f4d9394fa57
aslavinska <anastasija.slavinska@avl.com>
2023-02-09 10:43:45 +0000

    Adding codition check for wins function

--- run.py
+++ run.py
@@ -24,8 +24,8 @@ def get_user_choice():
     print("Enter your choice \n 1 - Rock \n 2 - Paper \n 3 - Scissors \n")
     # Take user input
     user_choice = int(input("Enter a your choice here: "))
-    while user_choice >3 or user_choice <1:
-        user_choice = int(input("Enter a valid choice please"))
+    while user_choice > 3 or user_choice < 1:
+        user_choice = int(input("Enter a valid choice please: "))
     if user_choice == 1:
         user_choice_name = 'Rock'
     elif user_choice == 2:
@@ -51,13 +51,52 @@ def get_computer_choice():
     print("Computer choice is \n", computer_choice_name)
     return computer_choice
 
+def condition_check():
+    user_selection = int(get_user_choice())
+    print(user_selection)
+    computer_selection = int(get_computer_choice())
+    print(computer_selection)
+    computerscore = 0
+    userscore = 0
+    # check draw
+    if user_selection == computer_selection:
+        print("It is a Draw \n", end="")
+    # condition for winning
+    if (user_selection == 1 and computer_selection == 2):
+        print("Player won, paper wins => \n", end="")
+        userscore = userscore + 1
+    elif (user_selection == 2 and computer_selection == 1):
+        print("Computer won, paper wins => \n", end="")
+        computerscore = computerscore + 1
+    if (user_selection == 1 and computer_selection == 3):
+        print("Player won, Rock wins=>\n", end="")
+        userscore = userscore + 1
+    elif (user_selection == 3 and computer_selection == 1):
+        print("Computer won, Rock wins=>\n", end="")
+        computerscore = computerscore + 1
+    if (user_selection == 2 and computer_selection == 3):
+        print("Player won, scissors win => \n", end="")
+        userscore = userscore + 1
+    elif (user_selection == 3 and computer_selection == 2):
+        print("Computer won, scissors win => \n", end="")
+        computerscore = computerscore + 1
+        
+    print("userscore: ", userscore)
+    print("computerscore: ", computerscore)
+    return userscore, computerscore
+    
+
 def main():
     """
     Run all program functions
     """
-    user_selection = get_user_choice()
-    print(user_selection)
-    computer_selection = get_computer_choice()
-    print(computer_selection)
+    while True:
+        condition_check()
+        print("Do you want to play again? (Y/N)")
+
+        ans = input().lower()
+        if ans == 'n':
+            break
+    print("thanks for playing")
 
 main()
